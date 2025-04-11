c4569ca42ec603977341daa1fb9d3b33b8742bde
Harshit Sharma <harhitsharma1032006@gmail.com>
2025-04-12 04:04:15 +0530

    msg

---
+++ src/switch_case.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class switch_case {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number ");
+
+        int a = sc.nextInt();
+        switch (a) {
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thuresday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("tum is desh ka nhi ho");
+        }
+    }
+
+
+}
