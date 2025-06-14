f92f68dff745fece85bb4d378fd1c2e4a0f56e3a
Harshit Sharma <182348654+Harshitsharma1022@users.noreply.github.com>
2025-06-14 22:19:58 +0530

    Delete modulusoperator.java

--- modulusoperator.java
+++
@@ -1,8 +0,0 @@
-//Find remainder using modulus operator
-class modulusoperator{
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 3;
-        System.out.println("Remainder: " + (a % b));
-    }
-}
