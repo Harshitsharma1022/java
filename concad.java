ae6cbca16a2231ef0609323f0ff4a2912a694e82
Harshit Sharma <182348654+Harshitsharma1022@users.noreply.github.com>
2025-06-14 21:34:08 +0530

    Delete concad.java

--- concad.java
+++
@@ -1,8 +0,0 @@
-class concad{
-    public static void main(String[] args){
-        System.out.println("Hello" + 10 + 20);
-        System.out.println(10 + 20 + "Hello");
-        System.out.println("Hello" + 10 + 10 + "Hii");
-    }
-
-}
